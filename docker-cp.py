0c154bb6b48d5e7869196f0b11dd2d55919eb2a8
Veaceslav <mindruv@redhat.com>
2017-01-23 16:36:12 -0500

    NOJIRA: wip, trying to add on the file unarchive

--- docker-cp.py
+++ docker-cp.py
@@ -27,6 +27,13 @@ class get_opts():
                           default=0,
                           type=int,
                           dest="buffersize")
+        parser.add_option("-c", "--create-archive",
+                          help="when specified together with copy from target"
+                          "container will create tar archive. Has no effect"
+                          "on copy from local path  to container",
+                          action="store_true",
+                          default=False,
+                          dest="create_archive")
         (self.options, self.args) = parser.parse_args()
         if len(self.args) != 2:
             print "{}\nError:\n\nIncorrect arguments counts\n"\
@@ -50,7 +57,7 @@ class get_opts():
 class docker_cp():
     """ docker_cp
     python implementation of docker cp command"""
-    def __init__(self, source, dest, buffsize):
+    def __init__(self, source, dest, buffsize, archive=False):
         self.client = docker_Client(base_url='unix://var/run/docker.sock')
         """ i know the bellow requirement for source and dest to be a list is
          dirty and has t be corrected """
@@ -60,6 +67,7 @@ class docker_cp():
         self.buffsize = buffsize
         self.docker_version = self.client.version()['ApiVersion']
         self.client.api_version
+        self.create_archive = archive
 
     def copy_from(self):
         """ copy data from container, perform API archive call,we will not
@@ -70,21 +78,14 @@ class docker_cp():
             self.dest = self.local_path+stat['name']+'.tar'
         else:
             self.dest = self.local_path+'.tar'
-        tarfile.open(mode="r|", fileobj=response_data).extractall()
-
-        """ not sure how to efficiently untar in memory without reding entire
-        tar, i guess i could find tar specifications read N amount of some sort
-        of metadata header size, that defined start,end and name of a member
-        and then extract data with seak and read similar to above.
-        Bellow is simple ugly method with untar in memory"""
-#        new_file = StringIO(response_data.data)
-#        tar = tarfile.open(mode="r", fileobj=new_file)
-#        for member in tar.getmembers():
-#            member = tar.extractfile(member)
-#            buf = '0'
-#            while buf != '':
-#                buf = member.read(4)
-#                print buf
+        if self.create_archive is True:
+            tarfile.open(mode="r|",
+                         fileobj=response_data).extractall(path=self.local_path)
+        elif self.create_archive is False:
+            return
+        else:
+            print "error, invalide create_archive value"
+            exit(1)
 
     def block_read(self, file, blocksize):
         while True:
@@ -117,8 +118,9 @@ if __name__ == "__main__":
     else this can be included as used externaly"""
     opts = get_opts()
     buffsize = opts.options.buffersize
+    create_archive = opts.options.create_archive
     if opts.copy_from_cont is True and opts.copy_to_cont is False:
-        cp = docker_cp(opts.arg1, opts.arg2, buffsize)
+        cp = docker_cp(opts.arg1, opts.arg2, buffsize, create_archive)
         cp.copy_from()
     elif opts.copy_to_cont is True and opts.copy_from_cont is False:
         cp = docker_cp(opts.arg2, opts.arg1, buffsize)
